71695a5c9fb774ca7da3d890c52f8506a9e2a343
SipanOhanyan <54571287+SipanOhanyan@users.noreply.github.com>
2024-11-13 19:37:45 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="signalrcore",
+    name="signalrcore-new",
     version="1.0.0",
-    author="mandrewcito",
-    author_email="anbaalo@gmail.com",
+    author="sipanohanyan",
+    author_email="sipanohanyan@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
     keywords="signalr core client 3.1",
     long_description=long_description,
