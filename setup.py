97dbbc5072732c53bdaf573f3808182307a0cc02
SipanOhanyan <54571287+SipanOhanyan@users.noreply.github.com>
2024-11-14 13:35:19 +0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="signalrcore-new",
+    name="signalrcore1",
     version="1.0.0",
     author="sipanohanyan",
     author_email="sipanohanyan@gmail.com",
