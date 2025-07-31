5dc9623944b764264e43cf4a6d06e5c22dbf9a26
Sipan Ohanyan <sipanohanyan@gmail.com>
2025-07-31 14:23:33 +0400

    Fix NoneType object reference exception

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore1",
-    version="1.1.4",
+    version="1.1.5",
     author="sipanohanyan",
     author_email="sipanohanyan@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
