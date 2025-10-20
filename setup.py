950ad77b556cc3ebe4be84c36cf35c7f4e3f544f
Sipan Ohanyan <sipanohanyan@gmail.com>
2025-10-20 16:25:11 +0400

    update packages versions

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="signalrcore1",
-    version="1.1.5",
+    version="1.1.6",
     author="sipanohanyan",
     author_email="sipanohanyan@gmail.com",
     description="A Python SignalR Core client(json and messagepack), with invocation auth and two way streaming. Compatible with azure / serverless functions. Also with automatic reconnect and manually reconnect.",
@@ -16,11 +16,11 @@ setuptools.setup(
     url="https://github.com/mandrewcito/signalrcore",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: 3.6"
+        "Programming Language :: Python :: 3.14"
     ],
     install_requires=[
         "requests>=2.32.3",
-        "websocket-client==1.8.0",
+        "websocket-client>=1.9.0",
         "msgpack==1.1.0"
     ]
 )
